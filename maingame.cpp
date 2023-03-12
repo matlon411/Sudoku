9b487989ce85eee9d455a0d1bcf623b589d109ae
matlon411 <mathias_lendal@hotmail.dk>
2023-03-12 12:21:50 +0100

    Added first functions

---
+++ maingame.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World";
+    return 0;
+}
+
+
+void create_field(){
+
+    return;
+}
+
+void update_field(){
+
+    return;
+}
+
+void print_game_board(){
+
+    return;
+}
